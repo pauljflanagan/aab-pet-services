2ba9f00786b5a6bcfeb73d1d1285e48aa0038bf8
paulflanagan <PaulFlanagan@protonmail.com>
2025-07-14 17:22:15 -0400

    Configure for GitHub Pages deployment

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
